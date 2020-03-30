563a57d2163163a76cf61b9f974b5c532594b757
Gabslocks <45531037+Gabslocks@users.noreply.github.com>
2020-03-30 21:36:26 +0300

    lab2 task3

---
+++ Lab2_txt.lisp
@@ -0,0 +1,7 @@
+(let ((in (open "./txt1.txt" :if-does-not-exist nil)))
+     (when in
+     (loop for line = (read-line in nil)
+     while line do (format t "~a~%" line))
+     (close in)
+     )
+)         
